a01c07f4e5f5af7f26e6d54fda4a270eea9addb3
chaemii <chaemiiii.4@gmail.com>
2025-07-14 19:41:47 +0900

    Initial Commit

---
+++ ADHDFocusApp/ADHDFocusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADHDFocusAppApp.swift
+//  ADHDFocusApp
+//
+//  Created by cham on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ADHDFocusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADHDFocusAppTests/ADHDFocusAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ADHDFocusAppTests.swift
+//  ADHDFocusAppTests
+//
+//  Created by cham on 7/14/25.
+//
+
+import Testing
+@testable import ADHDFocusApp
+
+struct ADHDFocusAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
